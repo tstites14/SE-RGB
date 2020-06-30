a523fb85534e70e9c6f16ee8b04c775d530610ce
Theodore Stites <argionad@gmail.com>
2020-06-30 18:10:40 -0400

    Add code to scroll through the HSV color model

--- SE-RGB/Program.cs
+++ SE-RGB/Program.cs
@@ -25,18 +25,45 @@ namespace IngameScript
 
         public Program()
         {
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
+
             Lights = new List<IMyInteriorLight>();
             GridTerminalSystem.GetBlocksOfType(Lights);
 
             //Filter Lights so only lights that specifically need RGB are part of the list
             Lights = Lights.Where(item => item.CustomData == "RGB").ToList();
-
-            Runtime.UpdateFrequency = UpdateFrequency.Update1;
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
             Echo($"Number of lights: {Lights.Count}");
+
+            foreach (var light in Lights)
+            {
+                light.Color = GetNextColor(light);
+                Echo($"Color: {light.Color}");
+            }
+        }
+
+        public Color GetNextColor(IMyInteriorLight currentLight)
+        {
+            Color currentColor = currentLight.Color;
+            Vector3 hsv = currentColor.ColorToHSVDX11();
+
+            hsv.Y = 1;
+            hsv.Z = 1;
+
+            if (hsv.X < 1)
+            {
+                hsv.X += 0.005f;
+            }
+            else
+            {
+                hsv.X = 0;
+            }
+
+            Echo(hsv.ToString());
+            return ColorExtensions.HSVtoColor(hsv);
         }
     }
 }
